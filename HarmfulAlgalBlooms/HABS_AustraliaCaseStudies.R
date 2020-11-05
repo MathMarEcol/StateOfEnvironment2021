c12b21de2d72cd92875481f351faddf8ff2a7d95
Jason Everett <jaseeverett@gmail.com>
2020-11-06 10:20:56 +1100

    Update HABs map

--- HarmfulAlgalBlooms/HABS_AustraliaCaseStudies.R
+++ HarmfulAlgalBlooms/HABS_AustraliaCaseStudies.R
@@ -28,20 +28,37 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 
 NRS_sf <- st_as_sf(NRS, coords = c("Longitude", "Latitude"), crs = 4326)
 
+HAB_sf <- read_csv("Data/HAB_Locations.csv", quote = "\"") %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  mutate(Lab_x = 112,
+         Lab_y = seq(-37, -48, length.out = 12),
+         Label2 = str_c(No, ") ", Location))
 
 gg <- ggplot() +
   geom_sf(data = world, size = 0.05, fill = "grey20") +
-  geom_sf(data = NRS_sf, colour = "red") +
-  geom_sf_text(data = NRS_sf, aes(label = Station), colour = "red", nudge_y = -1) +
-  scale_x_continuous(expand = c(0, 0), limits = c(100, 170)) +
-  scale_y_continuous(expand = c(0, 0), limits = c(-50, 0)) +
+  # geom_sf(data = NRS_sf, colour = "red") +
+  # geom_sf_text(data = NRS_sf, aes(label = Station), colour = "red", nudge_y = -1) +
+  geom_sf(data = HAB_sf, colour = "red", size = 2) +
+  geom_sf_text(data = HAB_sf, aes(label = No), colour = "red", position=position_jitter(width=1.2, height=1.2)) +
+  scale_x_continuous(expand = c(0, 0), limits = c(105, 165)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(-50, -5)) +
   theme_bw() +
-  theme(axis.title = element_blank())
+  theme(axis.title = element_blank()) +
+  geom_text(dat = HAB_sf, aes(x = Lab_x, y = Lab_y, label = Label2), size = 3, hjust = 0)
 
 graphics.off()
 x11(width = 8, height = 6)
 gg
 ggsave("Figures/HABs_CaseStudyMap.png", dpi = 500)
+
+
+gg <- ggplot() +
+  geom_sf(data = world, size = 0.05, fill = "grey20") +
+  scale_x_continuous(expand = c(0, 0), limits = c(110, 161)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(-45, -10)) +
+
+gg
+
 
 
 
